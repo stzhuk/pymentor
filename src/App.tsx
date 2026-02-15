2f4fecf81963ea99e8b4134c9515168727b3aba5
Petro <petro.stzhuk@icloud.com>
2026-02-15 10:35:01 +0100

    fix: sidebar navigation on HomePage, and fixed links for GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { IntroPage } from "./pages/IntroPage";
 import "./App.css";
 
 function App() {
   return (
-    <Router basename="/pymentor/">
+    <Router basename="/pymentor">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/intro" element={<IntroPage />} />
